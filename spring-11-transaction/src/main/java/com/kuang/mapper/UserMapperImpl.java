d0a4603c963322b97516117b96bfb254cfac4622
SuperAxFax <2787086427@qq.com>
2022-03-01 17:51:06 +0800

    声明式事务的配置

---
+++ spring-11-transaction/src/main/java/com/kuang/mapper/UserMapperImpl.java
@@ -0,0 +1,27 @@
+package com.kuang.mapper;
+
+import com.kuang.pojo.User;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.lang.UsesSunHttpServer;
+
+import java.util.List;
+
+public class UserMapperImpl extends SqlSessionDaoSupport implements UserMapper {
+
+    public List<User> selectUser() {
+        User user = new User(4, "小王", "123456");
+        UserMapper mapper = getSqlSession().getMapper(UserMapper.class);
+        mapper.addUser(user);
+        mapper.deleteUser(4);
+        return mapper.selectUser();
+    }
+
+    public int addUser(User user) {
+        return getSqlSession().getMapper(UserMapper.class).addUser(user);
+    }
+
+    public int deleteUser(int id) {
+        return getSqlSession().getMapper(UserMapper.class).deleteUser(id);
+    }
+}
